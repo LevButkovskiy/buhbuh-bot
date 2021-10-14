e08d54c5b62c07bb1ec1d9e28ca69d7a128f7d90
Lev Butkovskiy <levbut@gmail.com>
2021-10-14 21:23:45 +0300

    Бары в отдельном файле Координаты бара

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import fetch from "node-fetch";
 import Telegraf from 'telegraf'
 
+import {bars1, bars2, bars3, routes} from './src/data.js';
+
 const API_TOKEN = process.env.API_TOKEN
 const URL_STATICS = 'http://api.bluebeakstd.ru:3080/v1/buhbuh'
 
-const bot = new Telegraf.Telegraf(API_TOKEN)
+const bot = new Telegraf.Telegraf(API_TOKEN);
 
 const generateStartText = () => {
     const startText = 'Привет! Ты попал в бота BuhBuh.';
@@ -15,156 +17,6 @@ const generateStartText = () => {
     return `${startText}\n${whatWeCan}\n${actionDrink}\n${actionSales}`
 }
 
-const bars1 = [
-    {
-        title: 'Oversize pizza club',
-        address: 'Гражданская улица 13-15'
-    },
-    {
-        title: 'El Copitas Bar',
-        address: 'Колокольная 2/18'
-    },
-    {
-        title: 'Paloma Cantina',
-        address: 'Садовая ул. 8/7'
-    },
-    {
-        title: 'Krang pizza',
-        address: 'Гороховая 28',
-    },
-    {
-        title: 'Chuck',
-        address: 'Гороховая 41',
-    },
-    {
-        title: 'Bar 812',
-        address: 'Жуковского 11'
-    },
-    {
-        title: 'Barquinta',
-        address: 'Жуковского 7-9'
-    }
-];
-
-
-const bars2 = [
-    {
-        title: 'Commod',
-        address: 'Рубинштейна 1'
-    },
-    {
-        title: 'УЕБар',
-        address: 'ул Восстания 12'
-    },
-    {
-        title: 'Пробирка',
-        address: 'ул Ломоносова 2'
-    },
-    {
-        title: '«Хроники»',
-        address: 'пл. Восстания, 24'
-    },
-    {
-        title: 'Бар Dead Poets',
-        address: 'ул. Жуковского, 12'
-    },
-    {
-        title: 'Union',
-        address: 'Литейный пр., 55'
-    },
-    {
-        title: '«Терминал»',
-        address: 'ул. Белинского, 11'
-    },
-];
-
-const bars3 = [
-    {
-        title: 'Cocos Lounge Bar',
-        address: 'Лиговский Проспект 10'
-    },
-    {
-        title: 'Black Heart Bar',
-        address: 'на Гороховой'
-
-    },
-    {
-        title: '4 Head’s Bar',
-        address: 'на Ропшинской улице'
-    },
-    {
-        title: 'Mr. Toad',
-        address: 'на Коломенской улице'
-    },
-    {
-        title: 'Xander Bar',
-        address: 'отель Four Seasons, Адмиралтейская'
-    },
-]
-
-const bars4 = [
-    {
-        title: 'The Hat',
-        address: 'ул. Белинского, 9'
-    },
-    {
-        title: '«Продукты»',
-        address: 'наб. Фонтанки, 17'
-
-    },
-    {
-        title: 'Mishka Bar',
-        address: 'наб. Фонтанки, 40'
-    },
-    {
-        title: '«Цветочки»',
-        address: 'ул. Рубинштейна, 23'
-    },
-    {
-        title: 'Mitte',
-        address: 'ул. Рубинштейна, 27'
-    },
-    {
-        title: 'Wood Bar',
-        address: 'ул. Марата, 34'
-    },
-    {
-        title: 'Zing',
-        address: 'ул. Ломоносова, 14'
-    },
-    {
-        title: 'Poison',
-        address: 'ул. Думская, 9'
-    },
-    {
-        title: 'Kopen',
-        address: 'Конюшенная пл., 2'
-    },
-]
-
-const routes = [
-    {
-        name: 'Персональный 1',
-        bars: bars1,
-        tags: ['персональный', 'личный', 'второй', '2', '2.', '2.персональный', '2. персональный']
-    },
-    {
-        name: 'Персональный 2',
-        bars: bars2,
-        tags: ['персональный', 'личный', 'второй', '2', '2.', '2.персональный', '2. персональный']
-    },
-    {
-        name: 'Персональный 3',
-        bars: bars3,
-        tags: ['персональный', 'личный', 'второй', '2', '2.', '2.персональный', '2. персональный']
-    },
-    {
-        name: 'Маршрут "Поперечного"',
-        bars: bars4,
-        tags: ['поперечный', 'поперечного', 'дани поперечного', 'первый', '1', '1.', '1.маршрут "поперечного"', '1. поперечного', 'маршрут поперечного']
-    }
-]
-
 const renderBars = (bar, index) => {
     if (bar.address) {
         return `${index + 1}. ${bar.title} - ${bar.address} \n`;
@@ -173,12 +25,13 @@ const renderBars = (bar, index) => {
 }
 
 const barsText = (title, arr) => {
+    const footerMap = '\nВысылаем координаты первого бара:'
     let finalString = '';
     arr.forEach(element => {
         finalString += element;
     });
 
-    return title + finalString;
+    return title + finalString + footerMap;
 }
 
 const sendStatics = (username, name, country_code, date, action) => {
@@ -191,7 +44,6 @@ const sendStatics = (username, name, country_code, date, action) => {
     }
 
     let strData = JSON.stringify(data);
-    console.log(strData)
 
     fetch(URL_STATICS, {
         method: 'POST',
@@ -201,7 +53,7 @@ const sendStatics = (username, name, country_code, date, action) => {
         },
     })
         .then(result => result.json())
-        .then(data => console.log(data))
+        .then(data => {})
         .catch(err => console.error(`Не удалось получить статистику с ${URL_STATICS}`, err.message))
 }
 
@@ -226,6 +78,12 @@ bot.command('/sales', (ctx) => {
     ctx.reply('10% на все, везде. Промокод: BUHBUH10')
 })
 
+bot.command('/test', (ctx) => {
+    // sendStatics(ctx.message.from.username, ctx.message.from.first_name, ctx.message.from.language_code, ctx.message.date, ctx.message.text)
+    ctx.telegram.sendLocation(ctx.message.chat.id, 59.932441, 30.345683)
+
+})
+
 bot.on('text', (ctx) => {
     sendStatics(ctx.message.from.username, ctx.message.from.first_name, ctx.message.from.language_code, ctx.message.date, ctx.message.text)
 
@@ -235,6 +93,7 @@ bot.on('text', (ctx) => {
     if (filteredRoutes.length) {
         const route = filteredRoutes[randomNumber(0, filteredRoutes.length - 1)]
         ctx.reply(barsText(`${route.name}\n`, route.bars.map(renderBars)))
+        ctx.telegram.sendLocation(ctx.message.chat.id, route.bars[0].latitude, route.bars[0].longitude)
     } else {
         ctx.reply('Такого у нас нет. Попробуй другой')
     }
